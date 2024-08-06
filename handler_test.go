cf4a7d98e36c68fa6e789ded6ba09bca87cd0ef4
eggnocent <egiwiratamaaa@gmail.com>
2024-08-06 13:53:56 +0700

    handler

--- handler_test.go
+++ handler_test.go
@@ -1,15 +1,42 @@
 package belajargolangweb
 
 import (
+	"fmt"
 	"net/http"
 	"testing"
 )
 
 func TestHandler(t *testing.T) {
 
-	var handler http.HandlerFunc = func(writter http.ResponseWritter, request *http.Request) {
+	var handler http.HandlerFunc = func(writter http.ResponseWriter, request *http.Request) {
 		// logic
 		// writter untuk mengembalikan response
+		fmt.Fprint(writter, "hello world")
+	}
+	server := http.Server{
+		Addr:    "localhost:8080",
+		Handler: handler,
+	}
+	err := server.ListenAndServe()
+	if err != nil {
+		panic(err)
+	}
+}
 
+func TestServeMux(t *testing.T) {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
+		fmt.Fprint(writer, "hello world")
+	})
+	mux.HandleFunc("/hi", func(writer http.ResponseWriter, request *http.Request) {
+		fmt.Fprint(writer, "hi")
+	})
+	server := http.Server{
+		Addr:    "localhost:8080",
+		Handler: mux,
+	}
+	err := server.ListenAndServe()
+	if err != nil {
+		panic(err)
 	}
 }
