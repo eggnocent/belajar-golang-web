e50638d016943b83898428c7c276288e45b08340
eggnocent <egiwiratamaaa@gmail.com>
2024-08-06 14:11:02 +0700

    request

--- handler_test.go
+++ handler_test.go
@@ -31,9 +31,31 @@ func TestServeMux(t *testing.T) {
 	mux.HandleFunc("/hi", func(writer http.ResponseWriter, request *http.Request) {
 		fmt.Fprint(writer, "hi")
 	})
+
+	mux.HandleFunc("/images/", func(writer http.ResponseWriter, request *http.Request) {
+		fmt.Fprint(writer, "images")
+	})
+	mux.HandleFunc("/images/thumnails/", func(writer http.ResponseWriter, request *http.Request) {
+		fmt.Fprint(writer, "image thumbnails")
+	})
 	server := http.Server{
 		Addr:    "localhost:8080",
 		Handler: mux,
+	}
+	err := server.ListenAndServe()
+	if err != nil {
+		panic(err)
+	}
+}
+
+func TestRequest(t *testing.T) {
+	var handler http.HandlerFunc = func(writer http.ResponseWriter, request *http.Request) {
+		fmt.Fprintln(writer, request.Method)
+		fmt.Fprintln(writer, request.RequestURI)
+	}
+	server := http.Server{
+		Addr:    "localhost:8080",
+		Handler: handler,
 	}
 	err := server.ListenAndServe()
 	if err != nil {
