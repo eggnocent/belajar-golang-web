b2d13dffde3335a6e8b132ffd0a1479b4eaad480
eggnocent <egiwiratamaaa@gmail.com>
2024-08-07 11:33:40 +0700

    Add test response code

--- response_code_test.go
+++ response_code_test.go
@@ -9,10 +9,10 @@ import (
 )
 
 func ResponseCode(writer http.ResponseWriter, request *http.Request) {
-	nama := request.URL.Query().Get("name")
+	nama := request.URL.Query().Get("nama")
 	if nama == "" {
 		writer.WriteHeader(http.StatusBadRequest)
-		fmt.Fprintln(writer, "name is empty")
+		fmt.Fprintln(writer, "nama kosong")
 	} else {
 		fmt.Fprintf(writer, "hello %s", nama)
 	}
@@ -20,6 +20,20 @@ func ResponseCode(writer http.ResponseWriter, request *http.Request) {
 
 func TestResponseCode(t *testing.T) {
 	request := httptest.NewRequest("GET", "http://localhost:8080", nil)
+	recorder := httptest.NewRecorder()
+
+	ResponseCode(recorder, request)
+
+	response := recorder.Result()
+	body, _ := io.ReadAll(response.Body)
+
+	fmt.Println(response.StatusCode)
+	fmt.Println(response.Status)
+	fmt.Println(string(body))
+}
+
+func TestResponseCodeValid(t *testing.T) {
+	request := httptest.NewRequest("GET", "http://localhost:8080/?nama=egi", nil)
 	recorder := httptest.NewRecorder()
 
 	ResponseCode(recorder, request)
