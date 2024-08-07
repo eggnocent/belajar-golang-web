b0ee448438ce762fd2f1027e6860c8a47c74d3ee
eggnocent <egiwiratamaaa@gmail.com>
2024-08-07 14:07:13 +0700

    add golang embed

--- file_server_test.go
+++ file_server_test.go
@@ -1,6 +1,7 @@
 package belajargolangweb
 
 import (
+	"embed"
 	"net/http"
 	"testing"
 )
@@ -8,6 +9,25 @@ import (
 func TestFileServer(t *testing.T) {
 	directory := http.Dir("./resources")
 	fileserver := http.FileServer(directory)
+
+	mux := http.NewServeMux()
+	mux.Handle("/static/", http.StripPrefix("/static", fileserver))
+
+	server := http.Server{
+		Addr:    "localhost:8080",
+		Handler: mux,
+	}
+	err := server.ListenAndServe()
+	if err != nil {
+		panic(err)
+	}
+}
+
+// go:embed resources
+var resources embed.FS
+
+func TestFileServerGoEmbed(t *testing.T) {
+	fileserver := http.FileServer(http.FS(resources))
 
 	mux := http.NewServeMux()
 	mux.Handle("/static/", http.StripPrefix("/static", fileserver))
