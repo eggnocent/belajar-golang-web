b8d06e0f8b2a840bce0b1e3a59aaae352a7d3fd9
eggnocent <egiwiratamaaa@gmail.com>
2024-08-07 13:57:16 +0700

    fixing prefik url

--- file_server_test.go
+++ file_server_test.go
@@ -10,11 +10,14 @@ func TestFileServer(t *testing.T) {
 	fileserver := http.FileServer(directory)
 
 	mux := http.NewServeMux()
-	mux.Handle("/static/", fileserver)
+	mux.Handle("/static/", http.StripPrefix("/static", fileserver))
 
 	server := http.Server{
 		Addr:    "localhost:8080",
 		Handler: mux,
 	}
-	server.ListenAndServe()
+	err := server.ListenAndServe()
+	if err != nil {
+		panic(err)
+	}
 }
