7dd54738775a33cea0a997d3d653a8f7c04b72d7
eggnocent <egiwiratamaaa@gmail.com>
2024-08-07 11:39:04 +0700

    Add SetCookie

---
+++ cookies_test.go
@@ -0,0 +1,16 @@
+package belajargolangweb
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func SetCookie(writer http.ResponseWriter, request *http.Request) {
+	cookie := new(http.Cookie)
+	cookie.Name = "X-PZN-Name"
+	cookie.Value = request.URL.Query().Get("name")
+	cookie.Path = "/"
+
+	http.SetCookie(writer, cookie)
+	fmt.Fprint(writer, "suksek buat kuki")
+}
