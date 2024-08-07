55b39d27cb95eb1bb64771557d1a434dfe1c2ddf
eggnocent <egiwiratamaaa@gmail.com>
2024-08-07 16:01:59 +0700

    add template by directoory

--- template_test.go
+++ template_test.go
@@ -46,3 +46,22 @@ func TestSimpleHTMLFile(t *testing.T) {
 	body, _ := io.ReadAll(recorder.Result().Body)
 	fmt.Println(string(body))
 }
+
+func TemplateDirectory(writer http.ResponseWriter, request *http.Request) {
+	t, err := template.ParseGlob("./templates/*.gohtml")
+	if err != nil {
+		panic(err)
+	}
+
+	t.ExecuteTemplate(writer, "simple.gohtml", "Hello HTML Template")
+}
+
+func TestTemplateDirectory(t *testing.T) {
+	request := httptest.NewRequest(http.MethodGet, "http://localhost:8080", nil)
+	recorder := httptest.NewRecorder()
+
+	TemplateDirectory(recorder, request)
+
+	body, _ := io.ReadAll(recorder.Result().Body)
+	fmt.Println(string(body))
+}
