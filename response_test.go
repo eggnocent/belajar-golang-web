46e99cb92e0ca692cb7e69f70d2de224e038baa8
eggnocent <egiwiratamaaa@gmail.com>
2024-08-07 10:36:01 +0700

    Add Test Form Post

--- response_test.go
+++ response_test.go
@@ -2,7 +2,11 @@ package belajargolangweb
 
 import (
 	"fmt"
+	"io"
 	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
 )
 
 func FormPost(writer http.ResponseWriter, request *http.Request) {
@@ -12,4 +16,19 @@ func FormPost(writer http.ResponseWriter, request *http.Request) {
 	firstName := request.PostForm.Get("firstName")
 	lastName := request.PostForm.Get("lastName")
 	fmt.Fprintf(writer, "%s %s", firstName, lastName)
+}
+
+func TestFormPost(t *testing.T) {
+	requestBody := strings.NewReader("firstName=egi&lastName=wira")
+	request := httptest.NewRequest(http.MethodPost, "http://localhost:8080", requestBody)
+	request.Header.Add("Content-type", "application/x-www-form-url-encoded")
+
+	recorder := httptest.NewRecorder()
+
+	FormPost(recorder, request)
+
+	response := recorder.Result()
+	body, _ := io.ReadAll(response.Body)
+
+	fmt.Println(string(body))
 }
