f2df03eb446921ab816f21e3cb2615ee20e2a7ee
eggnocent <egiwiratamaaa@gmail.com>
2024-08-07 10:53:43 +0700

    Add TestResponseCode func

--- response_code_test.go
+++ response_code_test.go
@@ -2,7 +2,10 @@ package belajargolangweb
 
 import (
 	"fmt"
+	"io"
 	"net/http"
+	"net/http/httptest"
+	"testing"
 )
 
 func ResponseCode(writer http.ResponseWriter, request *http.Request) {
@@ -13,4 +16,18 @@ func ResponseCode(writer http.ResponseWriter, request *http.Request) {
 	} else {
 		fmt.Fprintf(writer, "hello %s", nama)
 	}
+}
+
+func TestResponseCode(t *testing.T) {
+	request := httptest.NewRequest("GET", "http://localhost:8080", nil)
+	recorder := httptest.NewRecorder()
+
+	ResponseCode(recorder, request)
+
+	response := request.Response
+	body, _ := io.ReadAll(response.Body)
+
+	fmt.Println(response.StatusCode)
+	fmt.Println(response.Status)
+	fmt.Println(body)
 }
