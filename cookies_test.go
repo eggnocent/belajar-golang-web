2f8d2bebd064876d2f44f9539835cdbd04c31b7e
eggnocent <egiwiratamaaa@gmail.com>
2024-08-07 11:57:29 +0700

    Add Test Cookie

--- cookies_test.go
+++ cookies_test.go
@@ -3,8 +3,10 @@ package belajargolangweb
 import (
 	"fmt"
 	"net/http"
+	"testing"
 )
 
+// cara menambahkan cookie ke respons
 func SetCookie(writer http.ResponseWriter, request *http.Request) {
 	cookie := new(http.Cookie)
 	cookie.Name = "X-PZN-Name"
@@ -15,11 +17,27 @@ func SetCookie(writer http.ResponseWriter, request *http.Request) {
 	fmt.Fprint(writer, "suksek buat kuki")
 }
 
+// membaca cookie dari request http
 func GetCookie(writer http.ResponseWriter, request *http.Request) {
 	cookie, err := request.Cookie("X-PZN-Name")
 	if err != nil {
 		fmt.Fprintf(writer, "No Cookie")
 	} else {
 		fmt.Fprintf(writer, "Hello %s", cookie.Value)
+	}
+}
+
+func TestCookie(t *testing.T) {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/set-cookie", SetCookie)
+	mux.HandleFunc("/set-cookie", SetCookie)
+
+	server := http.Server{
+		Addr:    "localhost:8080",
+		Handler: mux,
+	}
+	err := server.ListenAndServe()
+	if err != nil {
+		panic(err)
 	}
 }
