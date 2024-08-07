00dba970fc00cb1da829cad37b13333dd89820b4
eggnocent <egiwiratamaaa@gmail.com>
2024-08-07 14:20:45 +0700

    fixing embed

--- file_server_test.go
+++ file_server_test.go
@@ -2,40 +2,46 @@ package belajargolangweb
 
 import (
 	"embed"
+	"io/fs"
 	"net/http"
 	"testing"
 )
 
 func TestFileServer(t *testing.T) {
+
 	directory := http.Dir("./resources")
-	fileserver := http.FileServer(directory)
+	fileServer := http.FileServer(directory)
 
 	mux := http.NewServeMux()
-	mux.Handle("/static/", http.StripPrefix("/static", fileserver))
+	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 
 	server := http.Server{
 		Addr:    "localhost:8080",
 		Handler: mux,
 	}
+
 	err := server.ListenAndServe()
 	if err != nil {
 		panic(err)
 	}
+
 }
 
-// go:embed resources
+//go:embed resources
 var resources embed.FS
 
-func TestFileServerGoEmbed(t *testing.T) {
-	fileserver := http.FileServer(http.FS(resources))
+func TestFileServerGolangEmbed(t *testing.T) {
+	directory, _ := fs.Sub(resources, "resources")
+	fileServer := http.FileServer(http.FS(directory))
 
 	mux := http.NewServeMux()
-	mux.Handle("/static/", http.StripPrefix("/static", fileserver))
+	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 
 	server := http.Server{
 		Addr:    "localhost:8080",
 		Handler: mux,
 	}
+
 	err := server.ListenAndServe()
 	if err != nil {
 		panic(err)
