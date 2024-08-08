420221d5707f60888ada42344d18f23ba762383a
eggnocent <egiwiratamaaa@gmail.com>
2024-08-08 11:36:07 +0700

    add Template Struct and solving problem

--- template_data_test.go
+++ template_data_test.go
@@ -6,7 +6,6 @@ import (
 	"io"
 	"net/http"
 	"net/http/httptest"
-	"strings"
 	"testing"
 )
 
@@ -28,17 +27,37 @@ func TestTemplateDataMap(t *testing.T) {
 	fmt.Println(string(body))
 }
 
-func TemplateAction(writer http.ResponseWriter, request *http.Request) {
-	t := template.New("name.gohtml").Funcs(template.FuncMap{
-		"upper": strings.ToUpper,
-	})
-	t = template.Must(t.ParseFiles("./templates/name.gohtml"))
+type Page struct {
+	Title   string
+	Name    string
+	Address Address
+}
 
-	data := map[string]interface{}{
-		"Title":    "Template dari map",
-		"Nama":     "Crarrson",
-		"IsMember": true,
-		"Items":    []string{"item 1", "item 2", "item 3"},
-	}
-	t.ExecuteTemplate(writer, "name.gohtml", data)
+type Address struct {
+	Street   string
+	City     string
+	Province string
+}
+
+func TemplateDataStruct(writer http.ResponseWriter, request *http.Request) {
+	t := template.Must(template.ParseFiles("./templates/name.gohtml"))
+	t.ExecuteTemplate(writer, "name.gohtml", Page{
+		Title: "Guru",
+		Name:  "Mikel",
+		Address: Address{
+			Street:   "Jl. Kiringan",
+			City:     "Sleman",
+			Province: "D.I Yogyakarta",
+		},
+	})
+}
+
+func TestTemplateDataStruct(t *testing.T) {
+	request := httptest.NewRequest(http.MethodGet, "http://localhost:8080", nil)
+	recorder := httptest.NewRecorder()
+
+	TemplateDataStruct(recorder, request)
+
+	body, _ := io.ReadAll(recorder.Result().Body)
+	fmt.Println(string(body))
 }
