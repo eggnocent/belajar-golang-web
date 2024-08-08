939b454026c3e1d54c218c01e02caa6be03b97cf
eggnocent <egiwiratamaaa@gmail.com>
2024-08-08 09:55:25 +0700

    fix request in Func Template data

---
+++ template_data_test.go
@@ -0,0 +1,28 @@
+package belajargolangweb
+
+import (
+	"fmt"
+	"html/template"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TemplateData(writer http.ResponseWriter, request *http.Request) {
+	t := template.Must(template.ParseFiles("./templates/name.gohtml"))
+	t.ExecuteTemplate(writer, "name.gohtml", map[string]interface{}{
+		"Title": "Template dari map",
+		"Nama":  "Egi Wiratama",
+	})
+}
+
+func TestTemplateDataMap(t *testing.T) {
+	request := httptest.NewRequest(http.MethodGet, "http://localhost:8080", nil)
+	recorder := httptest.NewRecorder()
+
+	TemplateData(recorder, request)
+
+	body, _ := io.ReadAll(recorder.Result().Body)
+	fmt.Println(string(body))
+}
