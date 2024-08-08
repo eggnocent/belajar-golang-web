82cb100819741c556bc77a8b03c5bfcb93cfc883
eggnocent <egiwiratamaaa@gmail.com>
2024-08-08 13:18:40 +0700

    problem struct

---
+++ template_action.go
@@ -0,0 +1,36 @@
+// file: belajargolangweb.go
+package belajargolangweb
+
+import (
+	"fmt"
+	"html/template"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+type Page struct {
+	Title string
+	Name  string
+}
+
+// TemplateActionIf handles the HTTP request and renders the template
+func TemplateActionIf(writer http.ResponseWriter, request *http.Request) {
+	t := template.Must(template.ParseFiles("./templates/if.gohtml"))
+	t.ExecuteTemplate(writer, "if.gohtml", Page{
+		Title: "Guru",
+		Name:  "Mikel",
+	})
+}
+
+// TestTemplateDataActionIf tests the TemplateActionIf function
+func TestTemplateDataActionIf(t *testing.T) {
+	request := httptest.NewRequest(http.MethodGet, "http://localhost:8080", nil)
+	recorder := httptest.NewRecorder()
+
+	TemplateActionIf(recorder, request)
+
+	body, _ := io.ReadAll(recorder.Result().Body)
+	fmt.Println(string(body))
+}
