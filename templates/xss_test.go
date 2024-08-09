8225a34b5e09c06bb792581831d82014455f740d
eggnocent <egiwiratamaaa@gmail.com>
2024-08-09 10:56:58 +0700

    xss no render

--- templates/xss_test.go
+++ templates/xss_test.go
@@ -39,3 +39,32 @@ func TestTemplateAutoEscapeServer(t *testing.T) {
 		panic(err)
 	}
 }
+
+func TemplateAutoEscapeNoRendering(writer http.ResponseWriter, request *http.Request) {
+	myTemplates.ExecuteTemplate(writer, "post.gohtml", map[string]interface{}{
+		"Title": "Auto escape",
+		"Body":  template.HTML("<p>ini body<script>allert('helo')''</script></p>"),
+	})
+}
+
+func TestTemplateEmbedNoRendering(t *testing.T) {
+	request := httptest.NewRequest(http.MethodGet, "http://localhost:8080", nil)
+	recorder := httptest.NewRecorder()
+
+	TemplateAutoEscapeNoRendering(recorder, request)
+
+	body, _ := io.ReadAll(recorder.Result().Body)
+	fmt.Println(string(body))
+}
+
+func TestTemplateAutoEscapeServerNoRendering(t *testing.T) {
+	server := http.Server{
+		Addr:    "localhost:8080",
+		Handler: http.HandlerFunc(TemplateAutoEscapeNoRendering),
+	}
+
+	err := server.ListenAndServe()
+	if err != nil {
+		panic(err)
+	}
+}
