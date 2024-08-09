1df548dc77b9d3314aa95fc5d8403b81d9a4299b
eggnocent <egiwiratamaaa@gmail.com>
2024-08-09 14:38:08 +0700

    add test form

---
+++ upload_file_test.go
@@ -0,0 +1,23 @@
+package belajargolangweb
+
+import (
+	"net/http"
+	"testing"
+)
+
+func UploadForm(writer http.ResponseWriter, request *http.Request) {
+	myTemplates.ExecuteTemplate(writer, "upload_form.html", nil)
+}
+
+func TestUploadForm(t *testing.T) {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/form", UploadForm)
+	server := http.Server{
+		Addr:    "localhost:8080",
+		Handler: mux,
+	}
+	err := server.ListenAndServe()
+	if err != nil {
+		panic(err)
+	}
+}
