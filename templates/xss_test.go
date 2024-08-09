899caa7c803d946ebfc03406814aafb86938f0ea
eggnocent <egiwiratamaaa@gmail.com>
2024-08-09 13:44:26 +0700

    add redirect

--- templates/xss_test.go
+++ templates/xss_test.go
@@ -68,3 +68,32 @@ func TestTemplateAutoEscapeServerNoRendering(t *testing.T) {
 		panic(err)
 	}
 }
+
+func TemplateXSS(writer http.ResponseWriter, request *http.Request) {
+	myTemplates.ExecuteTemplate(writer, "post.gohtml", map[string]interface{}{
+		"Title": "Template Auto Escape",
+		"Body":  template.HTML(request.URL.Query().Get("body")),
+	})
+}
+
+func TestTemplateXSS(t *testing.T) {
+	request := httptest.NewRequest(http.MethodGet, "http://localhost:8080/?body=<p>alert</p>", nil)
+	recorder := httptest.NewRecorder()
+
+	TemplateXSS(recorder, request)
+
+	body, _ := io.ReadAll(recorder.Result().Body)
+	fmt.Println(string(body))
+}
+
+func TestTemplateXSSServer(t *testing.T) {
+	server := http.Server{
+		Addr:    "localhost:8080",
+		Handler: http.HandlerFunc(TemplateXSS),
+	}
+
+	err := server.ListenAndServe()
+	if err != nil {
+		panic(err)
+	}
+}
