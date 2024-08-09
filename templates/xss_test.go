26d6fdfb5b99bd25d6533672d68b823f21ca1ae3
eggnocent <egiwiratamaaa@gmail.com>
2024-08-09 10:50:50 +0700

    xss testing success

---
+++ templates/xss_test.go
@@ -0,0 +1,41 @@
+package templates
+
+import (
+	"fmt"
+	"html/template"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+var myTemplates = template.Must(template.ParseFiles("post.gohtml"))
+
+func TemplateAutoEscape(writer http.ResponseWriter, request *http.Request) {
+	myTemplates.ExecuteTemplate(writer, "post.gohtml", map[string]interface{}{
+		"Title": "Auto escape",
+		"Body":  "<p>ini body<script>allert('helo')''</script></p>",
+	})
+}
+
+func TestTemplateEmbed(t *testing.T) {
+	request := httptest.NewRequest(http.MethodGet, "http://localhost:8080", nil)
+	recorder := httptest.NewRecorder()
+
+	TemplateAutoEscape(recorder, request)
+
+	body, _ := io.ReadAll(recorder.Result().Body)
+	fmt.Println(string(body))
+}
+
+func TestTemplateAutoEscapeServer(t *testing.T) {
+	server := http.Server{
+		Addr:    "localhost:8080",
+		Handler: http.HandlerFunc(TemplateAutoEscape),
+	}
+
+	err := server.ListenAndServe()
+	if err != nil {
+		panic(err)
+	}
+}
