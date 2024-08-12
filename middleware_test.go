bf2e7a8a637973d31349d20e26edde137b156381
eggnocent <egiwiratamaaa@gmail.com>
2024-08-12 11:57:33 +0700

    final Middleware

--- middleware_test.go
+++ middleware_test.go
@@ -16,31 +16,51 @@ func (middleware *LogMiddleware) ServeHTTP(writer http.ResponseWriter, request *
 	fmt.Println("after execute handler")
 }
 
+type ErrorHandler struct {
+	Handler http.Handler
+}
+
+func (errorHandler *ErrorHandler) ServeHTTP(writer http.ResponseWriter, request *http.Request) { // Ubah request menjadi *http.Request
+	defer func() {
+		if err := recover(); err != nil { // Pastikan untuk menangkap panic
+			fmt.Println("ERROR:", err)
+			writer.WriteHeader(http.StatusInternalServerError)
+			fmt.Fprintf(writer, "error: %s", err)
+		}
+	}()
+	errorHandler.Handler.ServeHTTP(writer, request) // Tidak perlu menggunakan &request
+}
+
 func TestMiddleware(t *testing.T) {
 	mux := http.NewServeMux()
 
-	// Definisikan handler yang akan dipanggil
 	mux.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
-		fmt.Fprintln(writer, "Hello middleware")
+		fmt.Println("Handler Executed")
+		fmt.Fprint(writer, "Hello Middleware")
+	})
+	mux.HandleFunc("/foo", func(writer http.ResponseWriter, request *http.Request) {
+		fmt.Println("Foo Executed")
+		fmt.Fprint(writer, "Hello Foo")
+	})
+	mux.HandleFunc("/panic", func(writer http.ResponseWriter, request *http.Request) {
+		fmt.Println("Panic Executed")
+		panic("ups panic")
 	})
 
-	// Buat instance LogMiddleware
 	logMiddleware := &LogMiddleware{
 		Handler: mux,
+	}
+	errorHandler := &ErrorHandler{
+		Handler: logMiddleware,
 	}
 
 	server := http.Server{
 		Addr:    "localhost:8080",
-		Handler: logMiddleware,
+		Handler: errorHandler,
 	}
 
 	// Jalankan server
 	err := server.ListenAndServe()
-	if err != nil {
-		panic(err)
-	}
-	err = server.ListenAndServe()
-
 	if err != nil {
 		panic(err)
 	}
